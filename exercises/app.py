5796a3c5c1fcc7a97c61658e374531f2e90f4ec6
aboud20-meet <aboud20@meet.mit.edu>
2018-08-08 14:09:51 +0300

    meh

--- exercises/app.py
+++ exercises/app.py
@@ -3,7 +3,9 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'START HERE: CHANGE ME'
-
+def home_route():
+    return render_template('home.html')
+@app.route('/students/<int:id>')
+def student(id):
+    return render_template("student.html", name = 'william', student_id = id)
 app.run(debug=True)
